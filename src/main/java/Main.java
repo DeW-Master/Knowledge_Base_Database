71081b4309d728e4aca24e1d69d216d1dbade8fc
Dejian W <52977418+DeW-Master@users.noreply.github.com>
2021-06-14 11:38:27 -0400

    structure re-organize finished

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        DataBase dataBase = new DataBase("src/main/resources");
+
+        while(true){
+            Scanner s = new Scanner(System.in);
+            System.out.println("Input your query: ");
+            String query = s.nextLine();
+            System.out.println("Input type (1.bag 2.probability 3.certainty 4.polynomial 5.normal)");
+            String type = s.nextLine();
+            if (query.equals("exit"))
+                break;
+            Engine provenanceSemiring = new Engine(Integer.valueOf(type), dataBase);
+            try {
+                Table res = provenanceSemiring.calculate(query);
+                TablePrinter.print(res);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
